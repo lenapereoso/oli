6cc4a453e729d02497aa512f8a32928751ed416e
Renata Pereira ദ്ദി •⩊• ) <pereiraosoriorenata@gmail.com>
2025-08-11 16:32:56 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(230,166,190);
+}
+
+function draw() {
+  d = random(10,60)
+  fill(255,random(55,200))
+  ellipse(mouseX,mouseY,d,d);
+}
