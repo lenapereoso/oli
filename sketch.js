5a5a547b9da26713c889a6ecfc4ac2ab5f09a46b
Renata Pereira ദ്ദി •⩊• ) <pereiraosoriorenata@gmail.com>
2025-08-11 17:16:34 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@ function setup() {
 
 function draw() {
   d = random(10,60)
-  colorMode(HSB,360,100,100);
+    colorMode(HSB,360,100,100);
   fill(random(0,360),100,100);
   ellipse(mouseX,mouseY,d,d);
 }
